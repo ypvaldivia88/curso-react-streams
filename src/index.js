e6e3d6a512d514c317b04ceffe10d22c4f425f40
Yasmani Palmero <yasmany.07@gmail.com>
2019-09-24 00:51:38 -0500

    install redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/App';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
